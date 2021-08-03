49c04fb819afd2a992f2f481912b35b7dff11ca8
Breno Rev <silvabreno462@gmail.com>
2021-08-03 02:13:01 -0300

    Criando o simulador de PDF com IText

--- src/main/java/classes/objetoEnviarEmail.java
+++ src/main/java/classes/objetoEnviarEmail.java
@@ -1,5 +1,9 @@
 package classes;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.time.LocalDate;
 import java.util.Properties;
 
 import javax.mail.Address;
@@ -11,18 +15,20 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 public class objetoEnviarEmail {
 
 	final String username = "counterstrikerevgamer@gmail.com";
 	final String senha = "brenodev123";
-	
+
 	private String listaDestinarios = "";
 	private String nomeRemetente = "";
 	private String assunto = "";
 	private String mensagem = "";
-	
-	
-	
+
 	public objetoEnviarEmail(String listaDestinarios, String nomeRemetente, String assunto, String mensagem) {
 		this.listaDestinarios = listaDestinarios;
 		this.nomeRemetente = nomeRemetente;
@@ -30,11 +36,9 @@ public class objetoEnviarEmail {
 		this.mensagem = mensagem;
 	}
 
-
-
 	public void enviarEmail(boolean envioHTML) {
 		try {
-			
+
 			/* Olhe as configurações do SMTP do email */
 			Properties properties = new Properties();
 
@@ -76,23 +80,46 @@ public class objetoEnviarEmail {
 			message.setFrom(remetente); // De quem
 			message.setRecipients(Message.RecipientType.TO, toUser); // Para quem
 			message.setSubject(assunto); // Assunto do email
-			
-			// Se a condição for verdadeira, ele vai transformar a mensagem digitada em HTML e enviar
-			if(envioHTML) {
-				message.setContent(mensagem, "text/html; charset=utf-8"); 
+
+			// Se a condição for verdadeira, ele vai transformar a mensagem digitada em HTML
+			// e enviar
+			if (envioHTML) {
+				message.setContent(mensagem, "text/html; charset=utf-8");
 			}
 			// Caso não, a mensagem é enviada da forma que foi escrita.
 			else {
-			message.setText(mensagem); // Mensagem do email
+				message.setText(mensagem); // Mensagem do email
 			}
 			// Enviar a mensagem
 			Transport.send(message);
 
-		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Email enviado com sucesso!");
 	}
 
+	/*
+	 * Este método simula o PDF ou qualquer arquivo que vai ser enviado no email O
+	 * arquivo pode ser pego no banco de dados
+	 * Retorna um PDF com o texto do paragrafo.
+	 */
+	private FileInputStream simulardorPDF() throws Exception {
+		// Cria um novo documento da biblioteca do itext
+		Document document = new Document();
+		// Cria um novo arquivo chamado "anexo.pdf"
+		File file = new File("Anexo.pdf");
+		file.createNewFile();
+		// Cria um escritor de pdf recebendo como parametro o documento e o pdf 
+		PdfWriter.getInstance(document, new FileOutputStream(file));
+		
+		// Modifica o PDF
+		document.open();
+		document.add(new Paragraph("Conteúdo do PDF anexo com Java Mail"));
+		document.addAuthor("Breno Dev");
+		document.close();
+		
+		// Retorna um novo PDF
+		return new FileInputStream(file);
+	};
 }
